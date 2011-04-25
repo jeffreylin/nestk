ec83a629cfa4ef3ae46134b4273e9c5f567d69ce
Jeffrey Lin <lin.jeffrey@gmail.com>
2011-04-25 15:25:05 -0700

    Removed extraneous code - eheheheh...

--- tests/test-nite.cpp
+++ tests/test-nite.cpp
@@ -34,49 +34,6 @@ namespace opt
   ntk::arg<bool> high_resolution("--highres", "High resolution color image.", 0);
 }
 
-class MyBodyListener : public BodyEventListener
-{
-public:
-  MyBodyListener() : m_last_hand_pos_2d(0,0,0)
-  {
-
-  }
-
-  virtual void triggerEvent(const BodyEvent &event)
-  {
-    switch (event.kind)
-    {
-    case BodyEvent::PushEvent:
-      ntk_dbg(1) << "PushEvent detected!";
-      break;
-    case BodyEvent::WaveEvent:
-      ntk_dbg(1) << "WaveEvent detected!";
-      break;
-    case BodyEvent::SteadyEvent:
-      ntk_dbg(1) << "SteadyEvent detected!";
-      break;
-    default:
-      ntk_dbg_print(event, 1);
-    };
-  }
-
-  virtual void triggerHandPoint(const HandPointUpdate &hand_point)
-  {
-    QMutexLocker locker(&m_lock);
-    m_last_hand_pos_2d = hand_point.pos_2d;
-  }
-
-  cv::Point3f getLastHandPosInImage() const
-  {
-    QMutexLocker locker(&m_lock);
-    return m_last_hand_pos_2d;
-  }
-
-private:
-  mutable QMutex m_lock;
-  Point3f m_last_hand_pos_2d;
-};
-
 int main(int argc, char **argv)
 {
   // Parse command line options.
@@ -91,14 +48,8 @@ int main(int argc, char **argv)
   QApplication app (argc, argv);
   QDir::setCurrent(QApplication::applicationDirPath());
 
-  // Prepare body event listeners.
-  ///MyBodyListener body_event_listener;
-  ///BodyEventDetector detector;
-  ///detector.addListener(&body_event_listener);
-
   // Declare the frame grabber.
   NiteRGBDGrabber grabber;
-  ///grabber.setBodyEventDetector(&detector);
 
   // High resolution 1280x1024 RGB Image.
   if (opt::high_resolution())
@@ -150,38 +101,6 @@ int main(int argc, char **argv)
     imshow("users", debug_users);
     cv::waitKey(10);
   }
-/*
-    // Wait for a new frame, get a local copy and postprocess it.
-    grabber.waitForNextFrame();
-    grabber.copyImageTo(image);
-    post_processor.processImage(image);
-
-    // Get the last hand point position.
-    ///cv::Point3f handpoint = body_event_listener.getLastHandPosInImage();
-
-    // Prepare the depth view, with skeleton and handpoint.
-    cv::Mat1b debug_depth_img = normalize_toMat1b(image.depth());
-    if (image.skeleton())
-      image.skeleton()->drawOnImage(debug_depth_img);
-    ///circle(debug_depth_img, Point(handpoint.x, handpoint.y), 5, Scalar(0, 255, 255));
-
-    // Prepare the color view, with skeleton and handpoint.
-    cv::Mat3b debug_color_img;
-    image.mappedRgb().copyTo(debug_color_img);
-    if (image.skeleton())
-      image.skeleton()->drawOnImage(debug_color_img);
-    ///circle(debug_color_img, Point(handpoint.x, handpoint.y), 5, Scalar(0, 255, 255));
-
-    // Prepare the user mask view as colors.
-    cv::Mat3b debug_users;
-    image.fillRgbFromUserLabels(debug_users);
-
-    imshow("depth", debug_depth_img);
-    imshow("color", debug_color_img);
-    imshow("users", debug_users);
-    cv::waitKey(10);
-  }
-*/
 
   return app.exec();
 }
